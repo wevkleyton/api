746817f54b0e2894a1219c82f16c5a353a0a9475
Wev kleyton <kleyton.cpd@gmnail.com>
2025-02-10 15:26:13 -0300

    Codificacao de delite de medico e alteracao da abela no banco

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class Endereco {
 
@@ -19,7 +19,7 @@ public class Endereco {
     private String cidade;
     private String uf;
 
-    public Endereco(){}
+//    public Endereco(){}
     public Endereco(DadosEndereco endereco) {
         this.logradouro = endereco.logradouro();
         this.bairro = endereco.bairro();
@@ -28,5 +28,29 @@ public class Endereco {
         this.complemento = endereco.complemento();
         this.cidade = endereco.cidade();
         this.uf = endereco.uf();
+    }
+
+    public void atualizaInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null){
+            this.logradouro = dados.logradouro();
+        }
+        if (dados.bairro() != null){
+            this.bairro = dados.bairro();
+        }
+        if (dados.cep() != null ){
+            this.cep = dados.cep();
+        }
+        if(dados.uf() != null){
+            this.uf = dados.uf();
+        }
+        if(dados.cidade() != null){
+            this.cidade = dados.cidade();
+        }
+        if (dados.numero() != null){
+            this.numero = dados.numero();
+        }
+        if (dados.complemento() != null){
+            this.complemento = dados.complemento();
+        }
     }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -26,8 +27,10 @@ public class Medico {
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -36,4 +39,22 @@ public class Medico {
         this.especialidade = dados.especialidade();
     }
 
+    public void atualizarInformacoes(DadosAtualizacaoMedicos dados) {
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizaInformacoes(dados.endereco());
+        }
+
+
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
